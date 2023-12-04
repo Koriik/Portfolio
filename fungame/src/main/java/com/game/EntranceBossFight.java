4862216bdc5af87cf935952ee9c9cb5016a6c474
Koriik <alexfrklukas@gmail.com>
2023-12-03 19:27:10 -0600

    Starting to implement the entrance boss fight

---
+++ fungame/src/main/java/com/game/EntranceBossFight.java
@@ -0,0 +1,31 @@
+package com.game;
+
+public class EntranceBossFight {
+    private static final int BOSS_HEALTH = 200;
+    private static final int BOSS_DAMAGE = 3;
+    private static final double BOSS_ARMOR = 1.5;
+    public void bossInfo() {
+
+        System.out.println("This beast hails from one of the darkest"
+        + " places in the underworld where there is torrental rain of flames"
+        + " and screams of those whom burn for eternity, The sky permanatly filled with red"
+        + " clouds of demise, that are impenatrable to any known mortal");
+        
+    }
+
+    public void damageFromBoss() {
+
+    }
+
+    public void attackBoss() {
+
+    }
+
+    public void defendBoss() {
+
+    }
+
+    public void lootBoss() {
+
+    }
+}
